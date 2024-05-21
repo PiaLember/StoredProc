d0b3f5e0882251c28783acbd367ddfb664feac5c
PiaLember <pia.lember@gmail.com>
2024-05-21 15:08:44 +0300

    DbContext

--- StoredProc/Data/StoredProcDbContext.cs
+++ StoredProc/Data/StoredProcDbContext.cs
@@ -1,8 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using StoredProc.Models;
 
 namespace StoredProc.Data{
     public class StoredProcDbContext : DbContext
     {
-        
+        public StoredProcDbContext(DbContextOptions<StoredProcDbContext>options)
+            :base(options) { }
+
+        public DbSet<Employee> Employees {get; set; }
     }
 }
