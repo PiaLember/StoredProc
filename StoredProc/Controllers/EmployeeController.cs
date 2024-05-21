b048dae29e90ffa47d52ab97600c778f2ed5fa8a
PiaLember <pia.lember@gmail.com>
2024-05-21 15:59:46 +0300

    EmployeeController

--- StoredProc/Controllers/EmployeeController.cs
+++ StoredProc/Controllers/EmployeeController.cs
@@ -1,12 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StoredProc.Data;
+using StoredProc.Models;
 
 namespace TTHKStoredProc.Controllers
 {
     public class EmployeeController : Controller
     {
+        public StoredProcDbContext _context;
+
+        public EmployeeController
+            (
+                StoredProcDbContext context
+            )
+
+            {
+                _context = context;
+            }
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IEnumerable<Employee> SearchResult()
+        {
+            var result = _context.Employees
+                .FromSqlRaw<Employee>("spSearchEmployees")
+                .ToList();
+
+            return result;
         }
     }
 }
